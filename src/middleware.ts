8561ce3d34f9834e15048c472f799bf4eaa894f2
Anthony <c0033688@newcaste.ac.uk>
2024-05-01 22:09:21 +0100

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard',
-  '/',
-  '/players'
+  '/'
 ]);
 
 export default clerkMiddleware((auth, req) => {
